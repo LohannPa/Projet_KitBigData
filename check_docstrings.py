f8ab427477a233c33c47c966caf843387a033758
LohannPa <lee.byonghee@gmail.com>
2023-10-23 17:51:40 +0200

    ajout de methode  modifier statut + test unitaire de la methode  + ajout poetry + ajout pipeline de qualite de code

---
+++ check_docstrings.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import glob
+from pydocstyle import check
+
+# Use the GITHUB_WORKSPACE environment variable to get the root directory of the repository
+repository_root = os.environ.get('GITHUB_WORKSPACE', '.')
+
+# You can define a directory or pattern for Python files within the repository
+python_files_pattern = os.path.join(repository_root, '*.py')
+python_files = glob.glob(python_files_pattern)
+
+# Run docstring checks on the specified directory
+exit_code = check(python_files)
+sys.exit(exit_code)
